c5caa96c0ce7169567f1cf83c89fef66721dadf8
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2023-09-26 15:24:25 -0300

    Inicio portfolio

---
+++ src/components/Header/header.js
@@ -0,0 +1,9 @@
+import { DivPrincipal } from "./headerStyle"
+
+export const Header = () => {
+    return (
+            <DivPrincipal>
+                <h1>PORTFOLIO</h1>
+            </DivPrincipal>
+    )
+}
---
+++ src/components/Header/headerStyle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const DivPrincipal = styled.div`
+    border: 1px solid black;
+    background-color: red;
+    margin: 0;
+    padding: 0;
+`
